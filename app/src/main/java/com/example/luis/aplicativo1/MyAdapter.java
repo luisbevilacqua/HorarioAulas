a8823d4f33ee9ccc3207e1a0baa2fb0769888f08
luisbevilacqua <luisfernandobpv@gmail.com>
2016-06-23 11:41:40 -0300

    Adicionadas animações aos CardViews de horários

--- app/src/main/java/com/example/luis/aplicativo1/MyAdapter.java
+++ app/src/main/java/com/example/luis/aplicativo1/MyAdapter.java
@@ -4,6 +4,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,6 +20,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
 
     public static final int card_type = 0;
     public static final int text_type = 1;
+
+    private int lastPosition = -1;
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         public MyViewHolder(View itemView){
@@ -84,15 +89,32 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
             txtMateria.setText(aulas.get(listPosition).getDisciplina());
             txtProfessor.setText(aulas.get(listPosition).getProfessor());
             txtSala.setText(aulas.get(listPosition).getSala());
+            setAnimation(holder.itemView, listPosition);
         }
         else{
             TextViewHolder holder = (TextViewHolder) viewHolder;
             TextView txtDia = holder.txtDia;
 
             txtDia.setText(" " + aulas.get(listPosition).getDia() + " ");
+            setAnimation(holder.itemView, listPosition);
         }
+
     }
 
+    private void setAnimation(View viewToAnimate, int position)
+    {
+        // If the bound view wasn't previously displayed on screen, it's animated
+        if (position > lastPosition)
+        {
+            viewToAnimate.setTranslationY(250);
+            viewToAnimate.animate()
+                .translationY(0)
+                .setInterpolator(new DecelerateInterpolator(3.f))
+                .setDuration(700)
+                .start();
+            lastPosition = position;
+        }
+    }
     @Override
     public int getItemViewType(int position){
         return MyData.tipo[position];
