b90a4b32d3b4aa75e7359da74350dd090c029ea9
luisbevilacqua <luisfernandobpv@gmail.com>
2016-07-02 01:17:55 -0300

    TabLayout implementado ao projeto, adicionados fragments para implementaçao do menu hamburguer e para a interface de fretados e restaurante

--- app/src/main/java/com/example/luis/aplicativo1/MainActivity.java
+++ app/src/main/java/com/example/luis/aplicativo1/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.luis.aplicativo1;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -19,41 +27,72 @@ public class MainActivity extends AppCompatActivity {
     private static RecyclerView.Adapter adapter;
     private int[] tipo = new int[50];
 
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        recyclerView.setHasFixedSize(true);
+        //toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        aulas = new ArrayList<Aula>();
-        int j = 0;
-        for(int i = 0; i < MyData.disciplina.length; i++){
-            if(i==0||!MyData.dia[i].equals(MyData.dia[i-1])){
-                aulas.add(new Aula("","","","","",MyData.dia[i]));
-                MyData.tipo[j]=1;
-                j++;
-            }
-            aulas.add(new Aula(MyData.horas[i],MyData.minutos[i],
-                    MyData.disciplina[i],MyData.professor[i],MyData.sala[i],MyData.dia[i]));
-            MyData.tipo[j] = 0;
-            j++;
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+        viewPager.setCurrentItem(1);
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_menu_black_24dp);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_query_builder_black_24dp);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_directions_bus_black_24dp);
+        tabLayout.getTabAt(3).setIcon(R.drawable.ic_restaurant_menu_black_24dp);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HamburguerMenu(), "Menu");
+        adapter.addFragment(new Horarios(), "Horários");
+        adapter.addFragment(new Fretados(), "Fretados");
+        adapter.addFragment(new RestauranteUniversitario(), "Restaurante Universitario");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
         }
 
-        adapter = new MyAdapter(aulas, MyData.tipo);
-        recyclerView.setAdapter(adapter);
-        recyclerView.addOnItemTouchListener(
-                new RecyclerClickListener(this, new RecyclerClickListener.OnItemClickListener(){
-                    @Override
-                    public void onItemClick(View view, int position){
-                        Log.d("DEBUG", "Chegou! Item: " + position);
-                    }
-                })
-        );
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return null;
+        }
     }
     public void onCardClick(View view){
         Toast.makeText(this,"Teste",Toast.LENGTH_SHORT).show();
